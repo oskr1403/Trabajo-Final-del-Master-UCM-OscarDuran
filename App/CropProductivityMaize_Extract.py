03c87b7a807b764c3df9c0bc1b7878ecfbcfbd26
oskr1403 <oscarduran1403@hotmail.com>
2024-09-15 18:22:11 -0600

    Update CropProductivityMaize_Extract.py

--- App/CropProductivityMaize_Extract.py
+++ App/CropProductivityMaize_Extract.py
@@ -5,7 +5,6 @@ import time
 from dotenv import load_dotenv
 import cdsapi
 
-# Load environment variables (only needed if running locally with a .env file)
 if not os.getenv("GITHUB_ACTIONS"):
     load_dotenv()
 
@@ -14,11 +13,11 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = "us-east-2"
 BUCKET_NAME = "trabajofinalmasterucmoscarduran"
 
-# Ensure AWS credentials and region are correctly set
+# Credencial de AWS, asegurar que estan correctas
 if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY or not AWS_REGION:
-    raise ValueError("AWS credentials or region are not set properly.")
+    raise ValueError("Las credenciales de ASW no son correctas.")
 
-# Define dataset, variables, and years
+# Define los datasets, el año y las variables.
 dataset = "sis-agroproductivity-indicators"
 variables = ['crop_development_stage', 'total_above_ground_production', 'total_weight_storage_organs']
 years = ['2019', '2020', '2021', '2022', '2023']
@@ -26,27 +25,26 @@ years = ['2019', '2020', '2021', '2022', '2023']
 client = cdsapi.Client()
 
 def wait_for_job_to_complete(client, request):
-    """Wait until the job is completed before attempting to download."""
+    """Espera hasta que el proceso haya terminado."""
     while True:
         try:
             client.retrieve(dataset, request)
-            break  # Exit the loop if the job is complete and ready for download
+            break  # Termina el ciclo si el trabajo esta descargado y listo
         except Exception as e:
-            if "Result not ready, job is running" in str(e):
-                print("Job is still running, waiting for 10 seconds before checking again...")
-                time.sleep(100)  # wait for 100 seconds before checking again
+            if "Trabajo en proceso" in str(e):
+                print("El trabajo se sigue procesando esperar 100 segundos para revisar de nuevo...")
+                time.sleep(100)  # Esperar 100 segundo antes de revisar de nuevo
             else:
                 raise  # Raise any other exceptions
 
 def upload_to_s3(temp_file_path, s3_client, bucket_name, s3_key):
-    """Upload file to S3."""
+    """Carga el arhico al S3."""
     if os.path.getsize(temp_file_path) > 0:
         s3_client.upload_file(temp_file_path, bucket_name, s3_key)
         print(f"File uploaded to S3 bucket {bucket_name} with key {s3_key}")
     else:
-        print("Temporary file is empty. No data was retrieved.")
+        print("El archivo temporal está vacío, no se encontró información.")
 
-# Method 2: Temporary File in Batches by Year
 try:
     for var in variables:
         for year in years:
@@ -64,17 +62,17 @@ try:
 
             with tempfile.NamedTemporaryFile(delete=False) as temp_file:
                 temp_file_path = temp_file.name
-                print(f"Attempting to retrieve data for {var} in year {year} to temporary file: {temp_file_path}")
+                print(f"Intentando obtener la información para {var} en el año {year} al archivo temporal: {temp_file_path}")
 
-                # Wait for the job to complete before downloading
+                # Esperar por el job hasta que descargue
                 wait_for_job_to_complete(client, request)
 
-                # Retrieve data and download it directly to the temporary file
+                # Obtener la data y alojarla en un archivo temporal
                 response = client.retrieve(dataset, request)
                 response.download(temp_file_path)
-                print(f"Data download completed for {var} in year {year}. File saved to: {temp_file_path}")
+                print(f"Descarga de datos lista para {var} del año {year}. Archivo guardado en: {temp_file_path}")
 
-                # Upload the file to S3
+                # Subir la información a S3
                 s3_client = boto3.client(
                     's3',
                     aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -89,7 +87,7 @@ except Exception as e:
     print(f"Error: {e}")
 
 finally:
-    # Clean up the temporary file
+    # Remover el archivo temporal
     try:
         os.remove(temp_file_path)
     except:
